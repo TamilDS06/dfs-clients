5072722db2ee23fdb25f7ac0e0fb99fa18d6aecd
Tamilselvan Mani <mtsmech04@gmail.com>
2023-11-10 17:18:15 +0530

    file path changes

--- utilss.py
+++ utilss.py
@@ -5,8 +5,10 @@ from datetime import datetime
 import boto3
 from flask_bcrypt import Bcrypt
 
-METADATA_CSV_PATH= 'C:\\my_folder\\dfs_python_flask_aws\\csv_files\\FileMetaData.csv'
-USERS_CSV_PATH = 'C:\\my_folder\\dfs_python_flask_aws\\csv_files\\user_data.csv'
+current_script_directory = os.path.dirname(__file__)
+project_root = os.path.abspath(os.path.join(current_script_directory, '..'))
+METADATA_CSV_PATH= project_root+"\\dfs_python_flask_aws\\csv_files\\FileMetaData.csv"
+USERS_CSV_PATH = project_root+"\\dfs_python_flask_aws\\csv_files\\user_data.csv"
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = os.environ.get("AWS_REGION")
